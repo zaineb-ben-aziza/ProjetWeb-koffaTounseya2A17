eaf437551e610cf4d54c1fa9fc71dfffd1c5a281
ademnsir <nsir.adem@esprit.tn>
2021-10-26 22:53:07 +0200

    aa

---
+++ Documents/copie3/main.cpp
@@ -0,0 +1,19 @@
+#include "mainwindow.h"
+#include <QApplication>
+#include <QMessageBox>
+#include "connection.h"
+#include<iostream>
+
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow w;
+    Connection c;
+    bool test=c.createconnect();
+    if(test)
+    {w.show();}
+
+
+    return a.exec();
+}
